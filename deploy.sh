bf90faa4634992609a5a237a14a249fc6e059f10
wstier <3114226144@qq.com>
2025-03-16 20:18:43 +0800

    激活虚拟环境

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,9 @@ cd /home/wstier477/zwky_api
 
 # 激活虚拟环境（虚拟环境可能位置不同，根据实际情况修改）
 
- source /home/wstier477/virtualenvs/ENV_DIR/bin/activate
+ cd /home/wstier477/virtualenvs/ENV_DIR/bin
+ source activate
+ cd /home/wstier477/zwky_api
  
 # 拉取最新代码
 echo "拉取最新代码..."
