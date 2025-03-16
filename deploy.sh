ecd7e85e6718bed10ba479ca48cbb8afd8b45a27
wstier <3114226144@qq.com>
2025-03-16 19:34:59 +0800

    更新部署脚本中的虚拟环境激活路径，以适应新的环境配置

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,11 @@ cd /home/wstier477/zwky_api
 
 # 激活虚拟环境（虚拟环境可能位置不同，根据实际情况修改）
 
+<<<<<<< HEAD
  source ../ENV_DIR/bin/activate
+=======
+ source /home/wstier477/ENV_DIR/bin/activate
+>>>>>>> 99da2fa (更新部署脚本中的虚拟环境激活路径，以适应新的环境配置)
 
 # 拉取最新代码
 echo "拉取最新代码..."
