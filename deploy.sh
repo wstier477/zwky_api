e502a3d54e3a0eba0258ac9a6d7d66cd5e28adf5
wstier <3114226144@qq.com>
2025-03-16 19:59:33 +0800

    虚拟环境

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,8 @@ cd /home/wstier477/zwky_api
 
 # 激活虚拟环境（虚拟环境可能位置不同，根据实际情况修改）
 
-<<<<<<< HEAD
-<<<<<<< HEAD
- source ../ENV_DIR/bin/activate
-=======
- source /home/wstier477/ENV_DIR/bin/activate
->>>>>>> 99da2fa (更新部署脚本中的虚拟环境激活路径，以适应新的环境配置)
-=======
- source /home/wstier477/ENV_DIR/bin/activate
->>>>>>> 99da2fa (更新部署脚本中的虚拟环境激活路径，以适应新的环境配置)
-
+ source ~/.virtualenvs/ENV_DIR/bin/activate
+ 
 # 拉取最新代码
 echo "拉取最新代码..."
 git pull
